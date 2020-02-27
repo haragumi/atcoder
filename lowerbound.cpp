5b8217bc72a54463d668269ff6c50905ac7b637a
haragumi <58354448+haragumi@users.noreply.github.com>
2020-02-27 10:32:39 +0900

    Update lowerbound.cpp

--- lowerbound.cpp
+++ lowerbound.cpp
@@ -6,15 +6,11 @@ using namespace std;
 int main() {
     vector<int> vec = {1, 1, 2, 2, 4, 5, 5, 6, 8, 8, 8, 10, 15};
 
-    auto position = lower_bound(vec.begin(), vec.end(), 8);  // 8以上の要素のうち、最小の要素
-    int idx_lower = distance(vec.begin(), position);
-    cout << "vec[" << idx_lower << "] = " << vec[idx_lower] << "\n";
+    int position = lower_bound(vec.begin(), vec.end(), 8) - vec.begin();  // 8以上の要素のうち、最小の要素
+    cout << "vec[" << position << "] = " << vec[position] << "\n";
 
-    // cout << "vec[" << idx_lower << "] = " << *position << "\n"; でも良い
-
-    position = upper_bound(vec.begin(), vec.end(), 8);  // 8より大きい要素のうち、最小の要素
-    idx_lower = distance(vec.begin(), position);
-    cout << "vec[" << idx_lower << "] = " << vec[idx_lower] << "\n";
+    position = upper_bound(vec.begin(), vec.end(), 8) - vec.begin();  // 8より大きい要素のうち、最小の要素
+    cout << "vec[" << position << "] = " << vec[position] << "\n";
 
     cout << upper_bound(vec.begin(), vec.end(), 8) - lower_bound(vec.begin(), vec.end(), 8) << endl; // vec中の8の個数
     
